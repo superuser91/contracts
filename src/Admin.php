ef2bc693d17a5a4e59795abefa696213eec438c7
dungnt2 <dungnt2.ctv@gmail.com>
2022-02-07 10:57:36 +0700

    add admin interface

---
+++ src/Admin.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace Vgplay\Contracts;
+
+use Illuminate\Database\Eloquent\Relations\BelongsToMany;
+use Illuminate\Database\Eloquent\Relations\HasMany;
+
+interface Admin
+{
+    public function roles(): BelongsToMany;
+    public function roleUsers(): HasMany;
+    public function hasRole($name): bool;
+    public function hasPermission($name): bool;
+}
